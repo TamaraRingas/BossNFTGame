76fa9f8d593fa3fbd1cc0ef7333441e7445645ab
tamararingas <tamriasdev@gmail.com>
2022-02-15 09:28:14 +0200

    initialize CharacterAttributes

---
+++ MyEpicGame.sol
@@ -0,0 +1,23 @@
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "./libraries/Base64.sol";
+
+import "hardhat/console.sol";
+
+contract MyEpicGame is ERC721 {
+  struct CharacterAttributes {
+    uint256 characterIndex;
+    string name;
+    string imageURI;
+    uint256 maxHp;
+    uint256 attackDamage;
+  }
+}
